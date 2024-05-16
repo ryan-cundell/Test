d6616458b0c316689da93878e189240ee4db7e68
ryan-cundell <ryan.cundell@hotmail.com.au>
2024-05-16 20:20:11 +0800

    Add files via upload
    exercise 1

---
+++ main.py
@@ -0,0 +1,37 @@
+# Python introduction
+
+
+print("Hello world!")
+
+
+# Exercsie 1
+a=15
+b=30
+
+print(f"The sum is {(a+b)/2}")
+
+name = "Ryan"
+
+ben, alice, john = "apple", "orange", "banana"
+
+print("hello "*10)
+
+name, age = "Ryan", 27
+
+weekday = "Thursday "
+month = "May"
+daymonth = print(weekday + month)
+
+string1 = "Today is a good day"
+print(string1[3])
+print(string1[0:5])
+print(string1[0:])
+
+
+# Week 1 Exercise
+name = "Ryan"
+favourite_number = 9
+
+for i in range((favourite_number**2) +1):
+  print(i)
+
